9431207b465319d056ae34c50e21b9dd12f1fdd2
swawesome9598 <swawesome9598@gmail.com>
2018-02-04 21:26:56 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,86 @@
+const Discord = require("discord.js");
+const YTDL = require ("ytdl-core");
+
+const TOKEN = "NDA5ODU3MTIyNTUwMTUzMjE3.DVktkQ.5xwQCZhMT-VN_aUqiAke2e67lU4";
+const PREFIX = "debbot!"
+
+var bot = new Discord.Client();
+
+var servers = {};
+
+function play(connection, message){
+	var server = servers[message.guild.id];
+	
+	server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+	
+	server.queue.shift();
+	
+	server.dispatcher.on("end", function() {
+		if (server.queue[0]) play(connection, message);
+		else connection.disconnect();
+	});
+}
+
+bot.on("ready", function() {
+	console.log("Ready");
+});
+
+bot.on("message", function(message) {
+	if (message.author.equals(bot.user)) return;
+	
+	if (!message.content.startsWith(PREFIX)) return;
+	
+	var args = message.content.substring(PREFIX.length).split(" ");
+	
+	switch (args[0]) {
+		case "ping":
+		   message.channel.sendMessage("Pong! :ping_pong:");
+		   break;   
+		case "pong":
+		   message.channel.sendMessage("Ping! :ping_pong:");
+		   break;  
+		case "info":
+		   message.channel.sendMessage("Im a bot created by Ishi ( Ishi@3514 )");
+		   break;
+		case "profile":
+		   message.channel.sendMessage(message.author.avatarURL);
+		   break;       
+		case "help":
+		   message.channel.sendMessage("debbot!ping/debbot!pong/debbot!info/debbot!profile");
+		   break;
+		case "play":
+		   if (!args[1]) {
+			   message.channel.sendMessage("Please provide a link");
+			   return;
+		   }
+		   
+		   if(!message.member.voiceChannel) {
+			   message.channel.sendMessage("You must be in a voice channel");
+			   return;
+		   }
+		   
+		   if(!servers[message.guild.id]) servers[message.guild.id] = {
+			   queue: []
+		   };
+		   
+		   var server = servers[message.guild.id];
+		   
+		   server.queue.push(args[1]);
+		   
+		   if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
+			   play(connection, message);
+		   });
+		   break;
+		case "skip":
+           var server = servers[message.guild.id];
+		   
+		   if (server.dispatcher) server.dispatcher.end();
+           break;		
+	}
+	
+
+})
+
+bot.on("ready", () => bot.user.setGame("debbot!help")) 
+
+bot.login(TOKEN);
